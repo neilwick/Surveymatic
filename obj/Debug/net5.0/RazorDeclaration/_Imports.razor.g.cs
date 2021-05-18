80538fb980c33d898da8484e7eb556504f4572e5
Neil Wick <neil@wick.net>
2021-05-18 11:19:45 -0400

    Update obj directory

--- obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
@@ -13,70 +13,70 @@ namespace Surveymatic
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\web3r\surveytest\Surveymatic\_Imports.razor"
+#line 1 "d:\web3r\Surveymatic\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\web3r\surveytest\Surveymatic\_Imports.razor"
+#line 2 "d:\web3r\Surveymatic\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\web3r\surveytest\Surveymatic\_Imports.razor"
+#line 3 "d:\web3r\Surveymatic\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\web3r\surveytest\Surveymatic\_Imports.razor"
+#line 4 "d:\web3r\Surveymatic\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\web3r\surveytest\Surveymatic\_Imports.razor"
+#line 5 "d:\web3r\Surveymatic\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\web3r\surveytest\Surveymatic\_Imports.razor"
+#line 6 "d:\web3r\Surveymatic\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\web3r\surveytest\Surveymatic\_Imports.razor"
+#line 7 "d:\web3r\Surveymatic\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\web3r\surveytest\Surveymatic\_Imports.razor"
+#line 8 "d:\web3r\Surveymatic\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\web3r\surveytest\Surveymatic\_Imports.razor"
+#line 9 "d:\web3r\Surveymatic\_Imports.razor"
 using Surveymatic;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\web3r\surveytest\Surveymatic\_Imports.razor"
+#line 10 "d:\web3r\Surveymatic\_Imports.razor"
 using Surveymatic.Shared;
 
 #line default
