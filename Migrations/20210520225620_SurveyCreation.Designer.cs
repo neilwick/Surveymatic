cf845a9df4fdf9d10214b8c946f625ec5e586e9d
Neil Wick <neil@wick.net>
2021-05-20 19:02:08 -0400

    Fixed conflict (capitalization)

--- Migrations/20210520225620_SurveyCreation.Designer.cs
+++ Migrations/20210520225620_SurveyCreation.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Surveymatic.Data;
 
 namespace Surveymatic.Migrations
 {
     [DbContext(typeof(HelpContext))]
-    partial class HelpContextModelSnapshot : ModelSnapshot
+    [Migration("20210520225620_SurveyCreation")]
+    partial class SurveyCreation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210520225620_SurveyCreation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Surveymatic.Migrations
+{
+    public partial class SurveyCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
